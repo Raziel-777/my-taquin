807fecb48d724126d03cd5087c3d1228f20c5e1b
Raziel-777 <thibault.dams@gmail.com>
2018-09-07 00:02:24 +0200

    WIP5 resolve all taquin method

--- src/app/taquin/taquin.component.ts
+++ src/app/taquin/taquin.component.ts
@@ -77,7 +77,7 @@ export class TaquinComponent implements OnInit {
   // Method to switch voidCell with one good cell to switch (taquin classic)
   switch(cellToSwitch: TaquinCell): void {
     // Coordinates x = vertical array, y = horizontal array
-    const voidCoordinates = this.findVoidCell(9);
+    const voidCoordinates = this.findCoordinates(9);
     const xVoid = voidCoordinates[0];
     const yVoid = voidCoordinates[1];
     // Four movements condition for voidCell = cellToSwitch
@@ -96,17 +96,6 @@ export class TaquinComponent implements OnInit {
     }
   }
 
-  // Find coordinates of cell with his value
-  findVoidCell(x: number): Array<number> {
-    for (let i = 0; i < this.cellsArray.length; i++) {
-      for (let j = 0; j < this.cellsArray[i].length; j++) {
-        if (this.cellsArray[i][j].value === x) {
-          return [i, j];
-        }
-      }
-    }
-  }
-
   changePattern(pattern: string): void {
     for (const entries of this.cellsArray) {
       for (const entry of entries) {
@@ -114,9 +103,10 @@ export class TaquinComponent implements OnInit {
       }
     }
   }
+
   solvencyTaquin(): void {
     // Find void Cell coordinates and calculate number of moves to set at initial position
-    const voidCell = this.findVoidCell(9);
+    const voidCell = this.findCoordinates(9);
     const initVoidCell = [2, 2];
     const xVoidMove = initVoidCell[0] - voidCell[0];
     const yVoidMove = initVoidCell[1] - voidCell[1];
@@ -146,23 +136,209 @@ export class TaquinComponent implements OnInit {
     // If parity of voidCell moves = parity of total moves for others cells, solvency is true (cf: https://fr.wikipedia.org/wiki/Taquin)
     this.solvency = (moves + voidMove) % 2 === 0;
   }
-  async movement(value: number) {
-    const coordinates = this.findVoidCell(value);
-    const diffVert = coordinates[0] - this.initial[value][0];
-    const diffHori = coordinates[1] - this.initial[value][1];
-    console.log(diffVert);
-    console.log(diffHori);
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //            MAIN METHOD FOR RESOLVE TAQUIN        /////////////////////////////////////////////////////////////////////////
+  async resolve() {
+    let coordinates = [];
+    let value;
+    for (let i = 0; i < this.cellsArray.length; i++) {
+      for (let j = 0; j < this.cellsArray[i].length - 2; j++) {
+        value = this.cellsArray[i][j].value;
+        coordinates = this.findCoordinates(value);
+        if (coordinates !== this.initial[value]) {
+          await this.voidCellPositioning(value);
+          await this.movement(value, this.initial[value]);
+        }
+      }
+      // Coordinates of two last cells of 2 lines
+      const coord1 = [i, this.cellsArray[i].length - 1];
+      const coord2 = [i + 1, this.cellsArray[i + 1].length - 1];
+      // Value of two last cells in the taquin
+      const val1 = this.cellsArray[coord1[0]][coord1[1]].value;
+      const val2 = this.cellsArray[coord2[0]][coord2[1]].value;
+      // Value of two last cells of 2 lines // WARNING natural value start at 0, but in my taquin value start at 1, so we add 1
+      const initVal1 = i * this.cellsArray.length + this.cellsArray[i].length;
+      const initVal2 = (i + 1) * this.cellsArray.length + this.cellsArray[i].length;
+      if (val1 === initVal1 && val2 === initVal2) {
+        await this.move(1, 3);
+        await this.move(1, 2);
+      } else {
+        // Value of the last cell and his coordinates, this cell must not be at three bad position
+        // This is initVal2
+        // Value of three bad position
+        // This is val1, val2 and val3
+        const val3 = this.cellsArray[coord2[0]][coord2[1] - 1].value;
+        if (initVal2 === val1 || initVal2 === val2 || initVal2 === val3) {
+          await this.voidCellPositioning(initVal2);
+          await this.movement(initVal2, [coord2[0], coord2[1] - 1]);
+        }
+        if (this.cellsArray[coord1[0]][coord1[1]].value !== initVal1) {
+          await this.voidCellPositioning(initVal1);
+          await this.movement(initVal1, [coord1[0], coord1[1]]);
+        }
+        if (this.cellsArray[coord2[0]][coord2[1]].value !== initVal2) {
+          await this.voidCellPositioning(initVal2);
+          await this.movement(initVal2, [coord2[0], coord2[1]]);
+        }
+        await this.lastHook();
+      }
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //            MOVEMENTS        //////////////////////////////////////////////////////////////////////////////////////////////
+
+  // Method to move cell at his initial position with this.initial when voidCell is positioning
+  // N hori [1]; poscv/N monx: vertical;
+  async movement(value: number, finalCoordinates: Array<number>) {
+    let coordinates = this.findCoordinates(value);
+    let diffVert = coordinates[0] - finalCoordinates[0];
+    let diffHori = coordinates[1] - finalCoordinates[1];
     if (diffVert > 0 && diffHori > 0 && this.flag === 1) {
       await this.upRight(diffVert);
       await this.movingLeftByDown(diffHori);
+    } else if (diffVert > 0 && diffHori === 0 && this.flag === 1) {
+      await this.upRight(diffVert);
+    } else if (diffVert > 0 && diffHori > 0 && this.flag === -1) {
+      await this.upLeft(diffVert);
+      await this.move(1, 3);
+      await this.movingLeftByDown(diffHori - 1);
+    } else if (diffVert > 0 && diffHori === 0 && this.flag === -1) {
+      if (coordinates[1] === this.cellsArray[coordinates[0]].length - 1) {
+        await this.upLeft(diffVert);
+      } else if (coordinates[1] < this.cellsArray[coordinates[0]].length - 1) {
+        if (coordinates[0] === this.cellsArray.length - 1) {
+          await this.hookUp();
+          await this.upRight(diffVert);
+        } else if (coordinates[0] < this.cellsArray.length - 1) {
+          await this.hookDown();
+          await this.upRight(diffVert);
+        }
+      }
+    } else if (diffVert > 0 && diffHori < 0 && this.flag === 1) {
+      if (coordinates[0] === this.cellsArray.length - 1) {
+        await this.move(1, 4);
+        await this.movingRightByTop(-diffHori - 1);
+        coordinates = this.findCoordinates(value);
+        diffVert = coordinates[0] - finalCoordinates[0];
+        if (coordinates[1] < this.cellsArray[coordinates[0]].length - 1) {
+          await this.hookUp();
+          await this.upRight(diffVert);
+        } else if (coordinates[1] === this.cellsArray[coordinates[0]].length - 1) {
+          await this.upLeft(diffVert);
+        }
+      } else if (coordinates[0] < this.cellsArray.length - 1) {
+        await this.move(1, 4);
+        await this.movingRightByDown(-diffHori - 1);
+        coordinates = this.findCoordinates(value);
+        diffVert = coordinates[0] - finalCoordinates[0];
+        if (coordinates[1] < this.cellsArray[coordinates[0]].length - 1) {
+          await this.hookDown();
+          await this.upLeft(diffVert);
+        } else if (coordinates[1] === this.cellsArray[coordinates[0]].length - 1) {
+          await this.upLeft(diffVert);
+        }
+      } else if (diffVert > 0 && diffHori < 0 && this.flag === -1) {
+        if (coordinates[0] === this.cellsArray.length - 1) {
+          await this.movingRightByTop(-diffHori);
+          coordinates = this.findCoordinates(value);
+          diffVert = coordinates[0] - finalCoordinates[0];
+          if (coordinates[1] < this.cellsArray[coordinates[0]].length - 1) {
+            await this.hookUp();
+            await this.upRight(diffVert - 1);
+          } else if (coordinates[1] === this.cellsArray[coordinates[0]].length - 1) {
+            await this.upLeft(diffVert);
+          }
+        } else if (coordinates[0] < this.cellsArray.length - 1) {
+          await this.movingRightByDown(-diffHori);
+          coordinates = this.findCoordinates(value);
+          diffVert = coordinates[0] - finalCoordinates[0];
+          if (coordinates[1] < this.cellsArray[coordinates[0]].length - 1) {
+            await this.hookDown();
+            await this.upRight(diffVert);
+          } else if (coordinates[1] === this.cellsArray[coordinates[0]].length - 1) {
+            await this.upLeft(diffVert);
+          }
+        }
+      } else if (diffVert === 0 && diffHori < 0 && this.flag === 1) {
+        if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.move(1, 4);
+          await this.movingRightByTop(-diffHori - 1);
+        } else if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.move(1, 4);
+          await this.movingRightByDown(-diffHori - 1);
+        }
+      } else if (diffVert === 0 && diffHori < 0 && this.flag === -1) {
+        if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.movingRightByTop(-diffHori);
+        } else if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.movingRightByDown(-diffHori);
+        }
+      } else if (diffVert === 0 && diffHori > 0 && this.flag === 1) {
+        if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.movingLeftByTop(diffHori);
+        } else if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.movingLeftByDown(diffHori);
+        }
+      } else if (diffVert === 0 && diffHori > 0 && this.flag === -1) {
+        if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.move(1, 3);
+          await this.movingLeftByTop(diffHori - 1);
+        } else if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.move(1, 3);
+          await this.movingLeftByDown(diffHori - 1);
+        }
+      } else if (diffVert < 0 && diffHori > 0 && this.flag === 1) {
+        await this.downRight(-diffVert);
+        coordinates = this.findCoordinates(value);
+        diffHori = coordinates[1] - finalCoordinates[1];
+        if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.movingLeftByDown(diffHori);
+        } else if (coordinates[1] === this.cellsArray.length - 1) {
+          await this.movingLeftByTop(diffHori);
+        }
+      } else if (diffVert < 0 && diffHori > 0 && this.flag === -1) {
+        await this.downLeft(-diffVert);
+        coordinates = this.findCoordinates(value);
+        diffHori = coordinates[1] - finalCoordinates[1];
+        if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.move(1, 3);
+          await this.movingLeftByDown(diffHori - 1);
+        } else if (coordinates[1] < this.cellsArray.length - 1) {
+          await this.move(1, 3);
+          await this.movingLeftByTop(diffHori - 1);
+        }
+      } else if (diffVert < 0 && diffHori < 0 && this.flag === 1) {
+        await this.downRight(-diffVert);
+        await this.move(1, 4);
+        await this.movingRightByTop(-diffHori - 1);
+      } else if (diffVert < 0 && diffHori < 0 && this.flag === -1) {
+        await this.downLeft(-diffVert);
+        await this.movingRightByTop(-diffHori);
+      } else if (diffVert < 0 && diffHori === 0 && this.flag === 1) {
+        await this.downRight(-diffVert);
+      } else if (diffVert < 0 && diffHori === 0 && this.flag === -1) {
+        await this.downLeft(-diffVert);
+      }
     }
   }
+
+  // Last hook of each line
+  async lastHook() {
+    await this.move(1, 1);
+    await this.move(1, 3);
+    await this.move(1, 2);
+  }
+
   // Position the voidCell at right or left of the cellToMove (prefer right except when ycellToMove = max y)
   // 1: UP, 2: DOWN, 3: RIGHT, 4: LEFT
-  async voidCellPositioning() {
+  async voidCellPositioning(x: number) {
     this.goMove();
-    this.emptyCase = this.findVoidCell(9); // [x, y] voidCell
-    const coordCell = this.findVoidCell(1); // [x, y] cellToMove
+    this.emptyCase = this.findCoordinates(9); // [x, y] voidCell
+    const coordCell = this.findCoordinates(x); // [x, y] cellToMove
     const diffVert = this.emptyCase[0] - coordCell[0]; // =0 same line >0 void under cell <0 upper
     const diffHori = this.emptyCase[1] - coordCell[1]; // =0 same column >0 at right <0 at left
     if (diffVert >= 0 && diffHori > 0) {
@@ -219,6 +395,7 @@ export class TaquinComponent implements OnInit {
       this.flag = -1;
     }
   }
+
   // CLIMB OR DOWN ONE CELL
   // 1: UP, 2: DOWN, 3: RIGHT, 4: LEFT
   // VoidCell at left of the cellToUp
@@ -232,6 +409,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 1);
     }
   }
+
   async downLeft(x: number) {
     this.goMove();
     for (let i = 0; i < x; i++) {
@@ -242,6 +420,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 2);
     }
   }
+
   // VoidCell at right of the cellToUp
   async upRight(x: number) {
     this.goMove();
@@ -253,6 +432,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 1);
     }
   }
+
   async downRight(x: number) {
     this.goMove();
     for (let i = 0; i < x; i++) {
@@ -263,6 +443,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 2);
     }
   }
+
   // MOVING LEFT OR RIGHT ONE CELL
   // 1: UP, 2: DOWN, 3: RIGHT, 4: LEFT
   // Moving from top
@@ -275,6 +456,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 3);
     }
   }
+
   async movingRightByTop(x: number) {
     for (let i = 0; i < x; i++) {
       await this.move(1, 1);
@@ -284,6 +466,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 4);
     }
   }
+
   // Moving from down
   async movingLeftByDown(x: number) {
     for (let i = 0; i < x; i++) {
@@ -294,6 +477,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 3);
     }
   }
+
   async movingRightByDown(x: number) {
     for (let i = 0; i < x; i++) {
       await this.move(1, 2);
@@ -303,6 +487,7 @@ export class TaquinComponent implements OnInit {
       await this.move(1, 4);
     }
   }
+
   // Hook for move cellVoid around cell to move (up or down and right)
   async hookUp() {
     await this.move(1, 1);
@@ -310,12 +495,28 @@ export class TaquinComponent implements OnInit {
     await this.move(1, 3);
     await this.move(1, 2);
   }
+
   async hookDown() {
     await this.move(1, 2);
     await this.move(1, 3);
     await this.move(1, 3);
     await this.move(1, 1);
   }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //            UTILITIES        //////////////////////////////////////////////////////////////////////////////////////////////
+  // Find coordinates of cell with his value
+  findCoordinates(x: number): Array<number> {
+    for (let i = 0; i < this.cellsArray.length; i++) {
+      for (let j = 0; j < this.cellsArray[i].length; j++) {
+        if (this.cellsArray[i][j].value === x) {
+          return [i, j];
+        }
+      }
+    }
+  }
+
   move(diff, destination) {
     const self = this;
     return new Promise(function (resolve, reject) {
@@ -356,7 +557,7 @@ export class TaquinComponent implements OnInit {
         return;
       }
       self.goMove();
-    }, 300);
+    }, 1000);
   }
 }
 
